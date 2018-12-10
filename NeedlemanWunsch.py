f6a35b2e312054e1eda8437b0f263b0a0c975691
Markiian <matsiuk@ucu.edu.ua>
2018-12-10 20:05:05 +0200

    fixed neddleman wunsch algorithm (backtracking failure)

--- NeedlemanWunsch.py
+++ NeedlemanWunsch.py
@@ -7,7 +7,8 @@ mismatch = -1
 def cost(a,b):
 	if a==b:
 		return match
-	return mismatch
+	else:
+		return mismatch
 
 
 class Cell:
@@ -17,23 +18,34 @@ class Cell:
 
 	def __repr__(self):
 		return str(self.value)
+		# return str(self.parents)
 
 def NeedlemanWunsch(a,b):
-	MATRIX = [[None for i in range(len(b)+1)] for i in range(len(a)+1)]
+	MATRIX = [[None for _ in range(len(a)+1)] for _ in range(len(b)+1)]
+	
+
+	for i in  range(len(a)+1):
+		MATRIX[0][i] = Cell(i*gap, ["H"])
+
+	for i in  range(len(b)+1):
+		MATRIX[i][0] = Cell(i*gap, ["V"])
+
 	MATRIX[0][0] = Cell(0, [])
 
-	for i in range(1, len(a)+1):
-		for j in range(1, len(b)+1):
+
+
+	for i in range(1, len(b)+1):
+		for j in range(1, len(a)+1):
 			res = []
 
-			if MATRIX[i-1][j-1]!=None:
-				res.append([MATRIX[i-1][j-1].value + cost(a[i-1],b[j-1]), "D"])
+			# if MATRIX[i-1][j-1]!=None:
+			res.append([MATRIX[i-1][j-1].value + match if a[j-1] == b[i-1] else mismatch, "D"])
 
-			if MATRIX[i-1][j]!=None:
-				res.append([MATRIX[i-1][j].value + gap, "V"])
+			# if MATRIX[i-1][j]!=None:
+			res.append([MATRIX[i-1][j].value + gap, "V"])
 
-			if MATRIX[i][j-1]!=None:
-				res.append([MATRIX[i][j-1].value + gap, "H"])
+			# if MATRIX[i][j-1]!=None:
+			res.append([MATRIX[i][j-1].value + gap, "H"])
 
 			max_res = max(res, key = lambda x: x[0])
 			parents = []
@@ -51,62 +63,46 @@ def backtrack(MATRIX, a,b):
 	str_m = ""
 	str_b = ""
 
-	i = len(a)
-	j = len(b)
-	# pprint(MATRIX)
+	i = len(b)
+	j = len(a)
+	pprint(MATRIX)
 	print("Score:", MATRIX[i][j])
 	while MATRIX[i][j].parents!=[]:
+		print(i,j)
 		if MATRIX[i][j].parents[0]=="D":
 			i-=1
 			j-=1
-			str_a+=a[i]
-			str_b+=b[j]
-			if a[i]==b[j]:
+			str_a+=a[j]
+			str_b+=b[i]
+			if a[j]==b[i]:
 				str_m += "|"
 			else:
 				str_m += "*"
-		elif MATRIX[i][j].parents[0]=="V":
-			i-=1
+
+		elif MATRIX[i][j].parents[0]=="H":
+			j-=1
 			str_b += "-"
-			str_a += a[i]
+			str_a += a[j]
 			str_m += " "
 
 		else:
-			j-=1
+			i-=1
 			str_a += "-"
-			str_b += b[j]
+			str_b += b[i]
 			str_m += " "
 
 
 	print(str_a[::-1]+"\n"+str_m[::-1]+"\n"+str_b[::-1])	
 
 
-
-
+def NW(a,b):
+	backtrack(NeedlemanWunsch(a, b),a ,b)
 
 	# pprint(MATRIX)
 
 
-a="CCGGTG"
-b="CCT"
-backtrack(NeedlemanWunsch(a, b),a ,b)
-print("\n")
+NW("abc", "abc")
 
-a="AAABBB"
-b="BBBAAA"
-backtrack(NeedlemanWunsch(a, b),a ,b)
-print("\n")
+# NW("abc", "bcad")
 
-a = "CTTCATAATCAAACCTGATCACAATTGGTTGTCAGTTTATTGGGGCGTATTTCAGAAAGTCCGGTTTCGGTTCTCTGTTTTTGTGGGAATATGATTACT"
-b = "TATGTAATACTGCGTACGTGCATTGTAGGATATGACCGTCCTGACCGGCTTGCTTCACACCGGGCTTGCATCCGGCGACACGCCTACGTGCGGTTAGTA"
-backtrack(NeedlemanWunsch(a, b),a ,b)
-
-
-a = "ACCGT"
-b = "ACG"
-backtrack(NeedlemanWunsch(a,b),a,b)
-
-
-a = "NLGPSTKDFGKISESREFDNQ"
-b = "QLNQLERSFGKINMRLEDALV"
-backtrack(NeedlemanWunsch(a,b),a,b)
+# NW("bcad", "abc")
